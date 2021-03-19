83eb634a11ae3339cfeb2eb73b0ea7f83693bb79
twicky99 <tharindu.wic@gmail.com>
2021-03-19 20:02:38 +0530

    minor changes done

--- listtitle_and_listview/lib/main.dart
+++ listtitle_and_listview/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter ListView and ListTile',
       theme: ThemeData(
         // This is the theme of your application.
         //
